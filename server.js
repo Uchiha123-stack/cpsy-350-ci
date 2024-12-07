acdec1ce4d2ebd950eaca5a9c98760609ebab970
admin <admin@localhost.localdomain>
2024-12-06 22:27:35 -0700

    h

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:003sdksdaksham');
+  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:003sdsxcsham');
 });
 
 const server = app.listen(PORT, HOST);
